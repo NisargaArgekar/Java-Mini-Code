84b67050d2b44d83da3ce6c6dc3e4f36e28c7e76
NisargaArgekar <argekarnisarga015@gmail.com>
2025-11-30 12:02:52 +0530

    Implementation of showBalance and deposit

--- Java/BankingProgram.java
+++ Java/BankingProgram.java
@@ -1,7 +1,59 @@
+import java.util.Scanner;
 
 public class BankingProgram {
-    
+     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        double balance = 0;
+        boolean isRunning = true;
+        int choice;
+
+        while(isRunning) {
+            System.out.println();
+            System.out.println("**-----> BANKING PROGRAM <-----**");
+            System.out.println();
+
+            System.out.println("1. Show Balance");
+            System.out.println("2. Deposit");
+            System.out.println("3. Withdraw");
+            System.out.println("4. Exit");
+            System.out.println();
+
+            System.out.print("Enter your choice (1-4): ");
+            choice = scanner.nextInt();
+
+            switch(choice) {
+                case 1 -> showBalance(balance);
+                case 2 -> balance += deposit();
+                case 3 -> System.out.println("WITHDRAW");
+                case 4 -> isRunning = false;
+                default -> System.out.println("INVALID CHOICE");
+
+            }
+        }
         
+        scanner.close();
+
     }
+
+    static void showBalance(double balance) {
+        System.out.println();
+        System.out.printf("$%.2f\n", balance);
+    }
+
+    static double deposit() {
+        double amount;
+        System.out.print("Enter an amount to be deposited: ");
+        amount = scanner.nextDouble();
+
+        if(amount < 0) {
+            System.out.println("Amount can not be negative");
+            return 0;
+        } else {
+            return amount;
+        }
+    }
+
+
+
 }
+
